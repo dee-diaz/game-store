41f55556587175e349b618fa0fefcb4c3379d7fb
dee-diaz <diazthediablo@yahoo.com>
2023-01-19 18:18:06 -0300

    Final

---
+++ dist/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-sidebar'),
+    menuItem = document.querySelectorAll('.nav__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-sidebar_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-sidebar_active');
+        })
+    })
+})
